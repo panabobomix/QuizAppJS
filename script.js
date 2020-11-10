e571414a42af8217d81128a6fc55915991278d61
panabobomix <panabobomix@gmail.com>
2020-11-10 19:53:28 +0100

    first commit, buliding style css

---
+++ script.js
@@ -0,0 +1,46 @@
+const QuizData = [
+    
+    {
+        question: ' How old id Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c',
+
+    },
+    {
+        question: 'What is the most use programming language in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd',
+    },
+    {
+        question: 'Who is he President US?',
+        a:'Florin Pop',
+        b:'Donald Trump',
+        C:'Ivan Soldano',
+        d:'Mihai Andrei',
+        correct: 'b',
+
+    },
+    {
+        question:' What doeas HTML stand for?',
+        a:'Hypertext Markup Language',
+        b:'Cascading Style Sheet',
+        c:'Jason Obiect Notation',
+        d:'Helicopters Terminals Motorboats Lamborginis',
+        correct:'a'
+    },
+    {
+        question:'What yeras was JavaScript launched?',
+        a:'1996',
+        b:'1995',
+        c:'1994',
+        d:'none of the above',
+        correct:'d'
+    },
+
+]
